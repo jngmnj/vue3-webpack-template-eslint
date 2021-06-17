7783f2ade181a712b58ae6888357b4cd2d797a76
jngminj <exorbitance@naver.com>
2021-06-17 20:22:09 +0900

    'new'

---
+++ webpack.config.js
@@ -0,0 +1,76 @@
+const path = require('path')
+const HtmlPlugin = require('html-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
+const { VueLoaderPlugin } = require('vue-loader')
+
+module.exports = {
+  resolve: {
+    extensions: ['.js', '.vue'],
+    // 경로 별칭
+    alias: {
+      '~': path.resolve(__dirname, 'src'),
+      'assets': path.resolve(__dirname, 'src/assets')
+    }
+  },
+
+  // 파일을 읽어들이기 시작하는 진입점 설정
+  entry: './src/main.js',
+
+  // 결과물(번들)을 반환하는 설정
+  output: {
+    // path: path.resolve(__dirname, 'dist'),
+    // filename: 'main.js',
+    clean: true
+  },
+// path는 절대경로필요해서 nodejs에서 
+// 전역변수__dirname(현재파일이 있는 그 경로)와 dist를 합쳐줌
+// dist에 있는 main.js를 번들로 만들어서 내어줌
+
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        use: 'vue-loader'
+      },
+      {
+        test: /\.s?css$/,
+        use: [
+          // 순서중요!
+          'vue-style-loader',
+          'style-loader',
+          'css-loader',
+          'postcss-loader',
+          'sass-loader'
+        ]
+      },
+      {
+        test: /\.js$/,
+        use: [
+          'babel-loader'
+        ]
+      },
+      {
+        test: /\.(png|jpe?g|gif|webp)$/,
+        use: 'file-loader'
+      }
+    ]
+  },
+
+  // 번들링 후 결과물의 처리방식 등 다양한 플러그인들을 설정
+  plugins: [
+    new HtmlPlugin({
+      template: './index.html'
+    }),
+    new CopyPlugin({
+      patterns: [
+        { from: 'static' }
+      ]
+    }),
+    new VueLoaderPlugin()
+  ],
+
+  devServer: {
+    host: 'localhost'
+  }
+}
+
